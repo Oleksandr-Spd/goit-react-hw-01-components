2c0a8c9c18fde56e1719b4401eb731a8da67271f
Oleksandr-Spd <133060544+Oleksandr-Spd@users.noreply.github.com>
2023-11-07 18:41:12 +0200

    redy

---
+++ src/components/friendList/FriendList.jsx
@@ -0,0 +1,18 @@
+import friends from '../../data/friends.json';
+import { FriendsList } from './FriendList.styled';
+import { FriendsListItem } from './FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <FriendsList>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendsListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </FriendsList>
+  );
+};
---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,30 @@
+import data from '../../data/data.json';
+import PropTypes from 'prop-types';
+import {
+  StatisticItem,
+  StatisticsBox,
+  StatisticsList,
+  StatisticsTitle,
+} from './Statistics.styled';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <StatisticsBox>
+      {title && <StatisticsTitle>{title}</StatisticsTitle>}
+
+      <StatisticsList>
+        {stats.map(({ id, label, percentage }) => (
+          <StatisticItem key={id}>
+            <span className="label">{label}</span>
+            <span className="percentage">{percentage} %</span>
+          </StatisticItem>
+        ))}
+      </StatisticsList>
+    </StatisticsBox>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
